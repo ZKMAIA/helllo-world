3ad859e164a741cb5c07b6b00f9833d39c91a6c0
zkmaia <zkmaia@gmail.com>
2020-09-30 10:56:12 -0300

    add product service

---
+++ src/services/Products.service.ts
@@ -0,0 +1,5 @@
+import { Product } from '../shared/Table/Table.mockdata'
+import http from '../utils/http'
+
+export const getAllProducts = () => 
+    http.get<Product[]>('http://localhost:3024/products').then(res => res.data)
