47f07049438035cadd0ee4f7d86bb2693ee3da14
zkmaia <zkmaia@gmail.com>
2020-10-06 16:47:20 -0300

    add not-found-route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import './App.css';
 import HomeView from '../../views/HomeView';
+import NotFoundView from '../../views/NotFoundView';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" exact component={HomeView}>
-          </Route>
+          <Route path="/" exact component={HomeView}/>
+          <Route component={NotFoundView}/>
         </Switch>
       </BrowserRouter>
     </div>
